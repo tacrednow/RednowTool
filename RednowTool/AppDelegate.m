0262108fbd25f88cfbb4367275d809fbd854b20f
yineng <YN@yinengdemac-mini.local>
2015-10-10 11:22:42 +0800

    change little

--- RednowTool/AppDelegate.m
+++ RednowTool/AppDelegate.m
@@ -3,7 +3,7 @@
 //  RednowTool
 //
 //  Created by rednow on 15/10/6.
-//  Copyright © 2015年 rednow. All rights reserved.
+//  Copyright © 2015年 rednow. All rights reserved./////
 //
 
 #import "AppDelegate.h"
